73a9fc11dd115fb6cf46298b3d2c2fc40673ec14
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-01-21 00:41:58 +0530

    Time: 3 ms (57.02%) | Memory: 46.6 MB (65.79%) - LeetSync

---
+++ 3605-construct-the-minimum-bitwise-array-i/construct-the-minimum-bitwise-array-i.java
@@ -0,0 +1,31 @@
+import java.util.List;
+
+class Solution {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int n = nums.size();
+        int[] ans = new int[n];
+        
+        // Iterate through each prime number in the input list
+        for (int i = 0; i < n; i++) {
+            int target = nums.get(i);
+            boolean found = false;
+            
+            for (int x = 0; x <= target; x++) {
+                // The bitwise OR of x and x+1 changes the rightmost 0 in x to a 1.
+                if ((x | (x + 1)) == target) {
+                    ans[i] = x;
+                    found = true;
+                    // Since we found the smallest x, we exit the inner loop immediately.
+                    break; 
+                }
+            }
+            
+            // If no such x was found after checking all values up to target
+            if (!found) {
+                ans[i] = -1;
+            }
+        }
+        
+        return ans;
+    }
+}
