2f723c2b7717f209492683a46cef92de378be582
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-02-13 09:30:37 +0530

    Time: 2 ms (85.34%) | Memory: 48 MB (60.78%) - LeetSync

---
+++ 55-jump-game/jump-game.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int n = nums.length;
+        int maxR = 0;
+
+        for(int i=0;i<n;i++){
+            if(i > maxR){
+                return false;
+            }else{
+                maxR = Math.max(maxR, i + nums[i]);
+            }
+        }
+        return true;
+        
+    }
+}
