6484e022f16e6d34cbe6e7c658042f75a8521c13
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-03-15 23:46:56 +0530

    Time: 4 ms (96.08%) | Memory: 45 MB (52.35%) - LeetSync

---
+++ 71-simplify-path/simplify-path.java
@@ -0,0 +1,31 @@
+class Solution {
+    public String simplifyPath(String path) {
+        Stack<String> stack = new Stack<>();
+
+        String[] parts = path.split("/");
+
+        for(String p : parts){
+
+            if(p.equals("") || p.equals(".")){
+                continue;
+            }
+            else if(p.equals("..")){
+                if(!stack.isEmpty()){
+                    stack.pop();
+                }
+            }
+            else{
+                stack.push(p);
+            }
+        }
+
+        StringBuilder result = new StringBuilder();
+
+        for(String dir : stack){
+            result.append("/").append(dir);
+        }
+
+        return result.length() == 0 ? "/" : result.toString();
+        
+    }
+}
