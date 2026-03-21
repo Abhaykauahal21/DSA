e4ae9c95cc6706fbb4753efb8616104be245941b
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-03-21 18:45:46 +0530

    Time: 1 ms (98.69%) | Memory: 47.5 MB (34.40%) - LeetSync

---
+++ 724-find-pivot-index/find-pivot-index.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+        for(int i=0;i<nums.length;i++){
+            total += nums[i];
+        }
+        int left = 0;
+
+        for(int i=0;i<nums.length;i++){
+            if(2*left + nums[i] == total){
+                return i;
+            }
+            left+= nums[i];
+        }
+        return -1;
+        
+    }
+}
