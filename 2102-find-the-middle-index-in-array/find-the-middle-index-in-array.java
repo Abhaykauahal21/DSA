509c4f4f8122f70f31f7467533db4e702e75cd82
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-03-23 08:29:42 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (51.09%) - LeetSync

--- 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
+++ 2102-find-the-middle-index-in-array/find-the-middle-index-in-array.java
@@ -1,19 +1,19 @@
 class Solution {
     public int findMiddleIndex(int[] nums) {
+        // ek solid formula use krenge left = total - left - nums[i];
+
+        // totalsum find kro;
         int totalsum = 0;
-        int n = nums.length;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             totalsum += nums[i];
         }
-
         int leftsum = 0;
-        for(int i=0;i<n;i++){
+        for(int i=0;i<nums.length;i++){
             if(leftsum == totalsum - leftsum - nums[i]){
                 return i;
             }
             leftsum += nums[i];
         }
         return -1;
-        
     }
 }
