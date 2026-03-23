0cb9c2be3a2e88b986e9e20bb36b1f5f919bdf70
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-03-23 09:32:07 +0530

    Time: 27 ms (9.44%) | Memory: 51.3 MB (78.50%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,18 +1,23 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int[] freq = new int[k];
-        freq[0] = 1;          // empty prefix
-        int sum = 0;
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int n = nums.length;
+        map.put(0,1);
+        int sum =0;
         int count = 0;
 
-        for (int num : nums) {
-            sum += num;
-            int mod = sum % k;
-            if (mod < 0) mod += k;
-
-            count += freq[mod];
-            freq[mod]++;
+        for(int i=0;i<n;i++){
+            sum += nums[i];
+            int mod = sum % k; // some ka mode nikalo kya voh divisible hai 
+            if(mod < 0){    // handle negative mode
+                mod += k;
+            }
+            if(map.containsKey(mod)){
+                count += map.get(mod);
+            }
+            map.put(mod, map.getOrDefault(mod,0)+1);  // mode ko put krdo hashmap mai
         }
         return count;
+        
     }
 }
