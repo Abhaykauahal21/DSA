c0b655291d1a4a79a45846cfe40664b6aa499871
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-03-30 21:46:41 +0530

    Time: 9 ms (37.19%) | Memory: 48.8 MB (87.33%) - LeetSync

--- 56-merge-intervals/merge-intervals.java
+++ 56-merge-intervals/merge-intervals.java
@@ -1,29 +1,19 @@
-import java.util.*;
-
 class Solution {
     public int[][] merge(int[][] intervals) {
-        int n = intervals.length;
-        // sort karo array ko
-        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+        Arrays.sort(intervals, (a,b)->a[0]-b[0]);
 
-        int idx = 0; // index for merged intervals
+        List<int[]> result = new ArrayList<>();
 
-        for (int i = 1; i < n; i++) {
-            if (intervals[i][0] <= intervals[idx][1]) {
-                // overlap → merge with previous
-                intervals[idx][1] = Math.max(intervals[idx][1], intervals[i][1]);
-            } else {
-                // no overlap → move forward
-                idx++;
-                intervals[idx] = intervals[i];
+        for(int[] interval : intervals){
+            int n = result.size();
+
+            if(n==0 || result.get(n-1)[1] < interval[0]){
+                result.add(interval);
+            }else{
+                result.get(n-1)[1] = Math.max(result.get(n-1)[1], interval[1]);
             }
         }
-
-        // return only the merged part
-        int[][] ans = new int[idx + 1][2];
-        for (int i = 0; i <= idx; i++) {
-            ans[i] = intervals[i];
-        }
-        return ans;
+        return result.toArray(new int[result.size()][]);
+        
     }
 }
