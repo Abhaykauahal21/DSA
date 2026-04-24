c22e947e78233243de065277a2a9a93093ca8388
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-04-24 09:10:58 +0530

    Time: 1 ms (100.00%) | Memory: 43.7 MB (82.00%) - LeetSync

--- 3019-furthest-point-from-origin/furthest-point-from-origin.java
+++ 3019-furthest-point-from-origin/furthest-point-from-origin.java
@@ -3,25 +3,17 @@ class Solution {
         int r = 0;
         int l = 0;
         int blank = 0;
-        int ans = 0;
 
-        for(char c: moves.toCharArray()){
+        for(char c : moves.toCharArray()){
             if(c == 'R'){
                 r++;
-            }else if(c == 'L'){
+            } else if(c == 'L'){
                 l++;
-            }else if(c == '_'){
+            } else {
                 blank++;
             }
         }
-        int a = Math.abs((blank + r) - l);
-        int b = Math.abs((blank + l) -r);
 
-        if(a > b){
-            return a;
-        }else{
-            return b;
-        }
-        
+        return Math.abs(r - l) + blank;
     }
 }
