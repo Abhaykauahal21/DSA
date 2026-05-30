5092c751744f30e1bf5a3fbc340e7014ccc3aba7
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-05-31 01:10:16 +0530

    Time: 1 ms (100.00%) | Memory: 71.3 MB (56.77%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -1,25 +1,23 @@
 class Solution {
     public int firstMissingPositive(int[] nums) {
         int n = nums.length;
-
+        // [3,4,-1,1]
         for(int i = 0; i < n; i++) {
 
-            while(nums[i] > 0 &&
-                  nums[i] <= n &&
-                  nums[nums[i] - 1] != nums[i]) {
-
+            while(nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                //swap
                 int temp = nums[i];
                 nums[i] = nums[temp - 1];
                 nums[temp - 1] = temp;
             }
         }
-
+        // agar sahi index pr nhi h to vahi number rsturn
         for(int i = 0; i < n; i++) {
             if(nums[i] != i + 1) {
                 return i + 1;
             }
         }
 
-        return n + 1;
+        return n + 1; // iff all correct retun exta number
     }
 }
