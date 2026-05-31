e2b5ebd6838df9654423c1b10cff42040b4021c4
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-05-31 12:46:39 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (82.33%) - LeetSync

--- 48-rotate-image/rotate-image.java
+++ 48-rotate-image/rotate-image.java
@@ -1,37 +1,24 @@
 class Solution {
-    public void rotate(int[][] matrix) {
-        int n = matrix.length;
+    public void rotate(int[][] mat) {
+        int n = mat.length;
+        
+        // transpose 
         for(int i=0;i<n;i++){
             for(int j=i;j<n;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+                int temp = mat[i][j];
+                mat[i][j] = mat[j][i];
+                mat[j][i] = temp;
             }
         }
+
+        //revese
+
         for(int i=0;i<n;i++){
             for(int j=0;j<n/2;j++){
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[i][n-j-1];
-                matrix[i][n-j-1] = temp;
+                int temp = mat[i][j];
+                mat[i][j] = mat[i][n-j-1];
+                mat[i][n-j-1] = temp;
             }
         }
-
-        // Transpose the matrix
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = i; j < n; j++) {
-        //         int temp = matrix[i][j];
-        //         matrix[i][j] = matrix[j][i];
-        //         matrix[j][i] = temp;
-        //     }
-        // }
-
-        // // Reverse each row
-        // for (int i = 0; i < n; i++) {
-        //     for (int j = 0; j < n / 2; j++) {
-        //         int temp = matrix[i][j];
-        //         matrix[i][j] = matrix[i][n - j - 1];
-        //         matrix[i][n - j - 1] = temp;
-        //     }
-        // }
     }
 }
