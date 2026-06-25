8df0b44bddf7c1011400093d74d232f5be1b28d7
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-06-25 08:49:15 +0530

    Time: 5 ms (69.45%) | Memory: 43.8 MB (15.08%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,22 +1,16 @@
 class Solution {
-    public int hIndex(int[] nums) {
-        int n = nums.length;
-        int[] bucket = new int[n+1];
-
-        for(int c : nums){
-            if(c >= n){
-                bucket[n]++;
-            }else{
-                bucket[c]++;
-            }
-        }
-        int paper = 0;
-        for(int h = n; h >= 0; h--){
-            paper += bucket[h];
-            if(paper >= h){
+    public int hIndex(int[] citations) {
+        Arrays.sort(citations);
+        int n = citations.length;
+    
+        for(int i=0;i<n;i++){
+            int h = n-i;
+            if(citations[i] >= h){
                 return h;
             }
         }
+
+        
         return 0;
         
     }
