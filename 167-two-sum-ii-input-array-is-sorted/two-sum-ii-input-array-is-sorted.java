4e407611c99ec56781e753d00e043d089ea31d8e
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-07-06 09:53:25 +0530

    Time: 2 ms (95.87%) | Memory: 48.4 MB (42.24%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.java
@@ -3,17 +3,18 @@ class Solution {
         int i = 0;
         int j = numbers.length-1;
 
-        while(i<j){
-            int sum = numbers[i] + numbers[j];
-
-            if(sum > target){
+        while(i < j){
+            if(numbers[i] + numbers[j] > target){
                 j--;
-            }else if(sum < target){
+            }else if(numbers[i] + numbers[j] < target){
                 i++;
             }else{
-                return new int[] {i+1, j+1};
+                return new int[]{i+1, j+1};
             }
+           
+            
         }
-        return new int[] {-1,-1};
+        return new int[]{-1,-1};
+        
     }
 }
