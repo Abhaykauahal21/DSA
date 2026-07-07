6c5f9f4eb1e1899b9fc4ebc3ebb2f0325608570a
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-07-07 09:20:45 +0530

    Time: 33 ms (73.90%) | Memory: 59.5 MB (21.93%) - LeetSync

--- 15-3sum/3sum.java
+++ 15-3sum/3sum.java
@@ -1,41 +1,34 @@
 class Solution {
-    public void twoSumhelper(int f, int[] nums, List<List<Integer>> res){
-        int i = f+1;
-        int j = nums.length-1;
-
-        while(i<j){
-            int sum = nums[f] + nums[i] + nums[j];
-            if(sum > 0){
-                j--;
-            }else if(sum < 0){
-                i++;
-            }else{
-                res.add(Arrays.asList(nums[f], nums[i], nums[j]));
-                i++;
-                j--;
-
-                while(i<j && nums[i] == nums[i-1]){
-                    i++;
-                }
-                while(i<j && nums[j] == nums[j+1]){
-                    j--;
-                }
-            }
-        }
-    }
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
         Arrays.sort(nums);
+        List<List<Integer>> ans = new ArrayList<>();
 
-        for(int f=0;f<nums.length;f++){
-            if(nums[f] > 0){
-                break;
+        for(int i=0;i<nums.length-2;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
             }
-            if(f==0 || nums[f] != nums[f-1]){
-                twoSumhelper(f,nums,res);
+            int left = i+1;
+            int right = nums.length-1;
+            while(left < right){
+                int sum = nums[i]+ nums[left]+nums[right];
+                if(sum == 0){
+                   ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                   while(left < right && nums[left]== nums[left+1]){
+                    left++;
+                   }
+                   while(left< right && nums[right]== nums[right-1]){
+                    right--;
+                   }
+                   left++;
+                   right--;
+                } else if(sum <0){
+                    left++;
+                }else{
+                    right--;
+                }
             }
         }
-        return res;
+        return ans;
         
     }
 }
