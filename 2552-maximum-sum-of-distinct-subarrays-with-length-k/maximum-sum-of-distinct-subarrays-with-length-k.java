e5cbe7911deba199cda54325de79e87976b355f4
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-07-15 20:22:12 +0530

    Time: 33 ms (91.61%) | Memory: 98.4 MB (80.58%) - LeetSync

--- 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
+++ 2552-maximum-sum-of-distinct-subarrays-with-length-k/maximum-sum-of-distinct-subarrays-with-length-k.java
@@ -3,10 +3,11 @@ class Solution {
         int n = nums.length;
         long sum = 0;
         long maxsum = 0;
-        int left =0 ;
-
         Set<Integer> set = new HashSet<>();
-        for(int right = 0;right<n;right++){
+
+        int left = 0;
+
+        for(int right = 0; right < n;right++){
 
             while(set.contains(nums[right])){
                 set.remove(nums[left]);
@@ -14,19 +15,21 @@ class Solution {
                 left++;
             }
 
+
             set.add(nums[right]);
             sum += nums[right];
 
-            if(right - left + 1==k){
-                    maxsum = Math.max(sum, maxsum);
-                    set.remove(nums[left]);
-                    sum -=nums[left];
-                    left++;
+            if(right - left + 1 == k){
+
+                maxsum = Math.max(sum, maxsum);
+
+
+                set.remove(nums[left]);
+                sum -= nums[left];
+                left++;
             }
         }
         return maxsum;
-    
         
-       
     }
 }
