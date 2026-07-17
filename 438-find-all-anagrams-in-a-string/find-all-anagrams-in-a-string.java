61dcd51fa2b366bc101ec697dcf14b710d7ed877
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-07-17 16:44:36 +0530

    Time: 13 ms (43.38%) | Memory: 46.9 MB (46.80%) - LeetSync

--- 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,25 +1,24 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        ArrayList<Integer> ans = new ArrayList<>();
-
         int[] pcount = new int[26];
-        int[] wcount = new int[26];
+        int[] scount = new int[26];
 
-        int k = p.length();   //window size
+        List<Integer> ans = new ArrayList<>();
 
+        // freq of p
         for(char c : p.toCharArray()){
             pcount[c - 'a']++;
         }
 
-        for(int i = 0; i< s.length(); i++){
-            wcount[s.charAt(i)-'a']++;
+        for(int i=0;i<s.length();i++){
+            scount[s.charAt(i) - 'a']++;
 
-            if(i >= k){
-                wcount[s.charAt(i-k) - 'a']--;
+            if(i>=p.length()){
+                scount[s.charAt(i - p.length()) - 'a']--;
             }
 
-            if(Arrays.equals(wcount, pcount)){
-                ans.add(i-k+1);
+            if(Arrays.equals(pcount, scount)){
+                ans.add(i - p.length() +1);
             }
         }
         return ans;
