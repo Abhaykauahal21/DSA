d5710dd70d0dc8a3dd45891dd1b1849c81f69274
Abhay kaushal <153590377+Abhaykauahal21@users.noreply.github.com>
2026-07-25 22:30:25 +0530

    Time: 0 ms (100.00%) | Memory: 44.7 MB (67.04%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -1,20 +1,21 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        int low = 0;
         int n = nums.length;
-        int high = n-1;
+        int left = 0;
+        int right = n-1;
 
-        while(low <= high){
-            int mid = low + (high - low)/2;
+        while(left <= right){
+            int mid = left + (right-left)/2;
+
             if(nums[mid]==target){
                 return mid;
             }else if(nums[mid] < target){
-                low = mid+1;
+                left = mid+1;
             }else{
-                high = mid-1;
+                right = mid-1;
             }
         }
-        return low;
+        return left;
         
     }
 }
